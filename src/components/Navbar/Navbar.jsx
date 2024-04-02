52323f6399d506b2736e9650c0b96fde808dcaf8
nahidraz23 <nzamanraz@gmail.com>
2024-04-02 16:40:05 +0600

    basic navbar UI opacity added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     </div>
 
     return (
-        <div className="navbar bg-base-100 p-2">
+        <div className="navbar bg-base-100 p-2 bg-opacity-40">
             <div className="navbar-start">
                 <div className="dropdown">
                     <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
